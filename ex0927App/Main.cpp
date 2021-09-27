c3964f03b67d9345524449adaea32c4a85976f34
김재현 <njh0999@naver.com>
2021-09-27 10:41:50 +0900

    Main.cpp 파일에 PrintInteger()함수에 newline문자를 제거하고 1-100까지 3번 출력

--- ex0927App/Main.cpp
+++ ex0927App/Main.cpp
@@ -3,11 +3,25 @@ void PrintInteger(int n)
 {
 	for (int i = 0; i < n; ++i)
 		printf("Hello : %d\n", i);
+	printf("\n");
 }
 int main()
 {
 	PrintInteger(100);
+	PrintInteger(100);
+	PrintInteger(100);
 }
+
+//#include <stdio.h>
+//void PrintInteger(int n)
+//{
+//	for (int i = 0; i < n; ++i)
+//		printf("Hello : %d\n", i);
+//}
+//int main()
+//{
+//	PrintInteger(100);
+//}
 
 //#include <stdio.h>
 //int main()
