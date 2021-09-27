04d1368d46145cf66717b5e44e5be8523f014656
김재현 <njh0999@naver.com>
2021-09-27 11:44:17 +0900

    64비트로 컴파일 완료한 Point 구조체 정의 및 const 포인터변수 예제 연습 완료.

--- ex0927App/Main.cpp
+++ ex0927App/Main.cpp
@@ -1,16 +1,136 @@
 #include <stdio.h>
-void PrintInteger(int n)
+typedef struct _point
 {
-	for (int i = 0; i < n; ++i)
-		printf("Hello : %d\n", i);
-	printf("\n");
+	int x;
+	int y;
+} Point;
+void InitPoint(Point* p, int x, int y)
+{
+	p->x = x;
+	p->y = y;
+}
+void UninitPoint(Point* p)
+{
+	p->x = 0;
+	p->y = 0;
+}
+void PrintPoint(Point* p)
+{
+	printf("(%d, %d)\n", p->x, p->y);
+}
+void AddPoint(Point* p, int x, int y)
+{
+	p->x += x;
+	p->y += y;
 }
 int main()
 {
-	PrintInteger(100);
-	PrintInteger(100);
-	PrintInteger(100);
+	Point pt;
+	InitPoint(&pt, 2,3);
+
+	PrintPoint(&pt);
+
+	AddPoint(&pt, 3, 3);
+
+	PrintPoint(&pt);
+
+	UninitPoint(&pt);
 }
+
+//#include <stdio.h>
+//void Increment(int* pn) //출력매개변수
+//{
+//	++* pn;
+//}
+//void Print(const int* pn) //입력매개변수
+//{
+//	printf("int : %d\n", *pn); 
+//}
+//void Decrement(int* pn) //출력매개변수
+//{
+//	--* pn;
+//}
+//void Reset(int* pn) //출력매개변수
+//{
+//	*pn = 0;
+//}
+//int main()
+//{
+//	int n = 0;
+//
+//	Increment(&n);
+//	Increment(&n);
+//	Increment(&n);
+//	Print(&n);
+//
+//	Decrement(&n);
+//	Print(&n);
+//	
+//	Reset(&n);
+//	Print(&n);
+//}
+
+//#include <stdio.h>
+//void PrintArray(const int* arr, int size) //int* arr는 수정하지 않으므로 const를 사용한다.
+//{
+//	for (int i = 0; i < size; ++i)
+//		printf("[%d] : %d\n", i, arr[i]);
+//}
+//int main()
+//{
+//	int arr[3] = { 1,2,3 };
+//	int arr2[5] = { 100,200,300,400,500 };
+//
+//	PrintArray(arr, 3);
+//	PrintArray(arr2, 5);
+//	PrintArray(arr2 + 2, 2);
+//}
+
+//#include <stdio.h>
+//typedef struct _point
+//{
+//	int x;
+//	int y;
+//} Point;
+//void PrintPoint(Point* arg)
+//{
+//	printf("(%d, %d)\n", arg->x, arg->y);
+//}
+//int main()
+//{
+//	Point pt1 = { 2,3 };
+//
+//	PrintPoint(&pt1);
+//}
+
+//#include <stdio.h>
+//void PrintString(const char* s)
+//{
+//	printf("String : %s\n", s);
+//}
+//int main()
+//{
+//	char buf1[] = "Hello!";
+//	const char* buf2 = "Hello!";
+//
+//	PrintString(buf1);
+//	PrintString(buf2);
+//	PrintString("Hello!");
+//}
+
+//#include <stdio.h>
+//void PrintInteger(int n)
+//{
+//	for (int i = 0; i < n; ++i)
+//		printf("Hello : %d\n", i);
+//	printf("\n");
+//}
+//int main()
+//{
+//	PrintInteger(100);
+//	PrintInteger(100);
+//	PrintInteger(100);
+//}
 
 //#include <stdio.h>
 //void PrintInteger(int n)
